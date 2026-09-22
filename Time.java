91aebfe31b9f0bfd6d17f48b16153e5b527e7bca
luhao <2777096@learn.vsb.bc.ca>
2026-09-22 13:11:53 -0700

    Add files via upload

---
+++ Time.java
@@ -0,0 +1,15 @@
+public class Time{
+	
+	public static void printAmerican(int s){
+		System.out.println(day + "," + month + date + "," + year);
+		
+		}
+	
+	public static void main(String[] args){
+			String day = " Monday ";
+			int date = 14;
+			String month = " September ";
+			int year = 2026;
+			System.out.println(day + date + month +year);
+		}
+}
