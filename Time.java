9726a650ae74a814cc65dbbefc9f42cb50cef7de
luhao <2777096@learn.vsb.bc.ca>
2026-09-23 12:46:39 -0700

    Add files via upload

--- Time.java
+++ Time.java
@@ -1,15 +1,19 @@
 public class Time{
 	
-	public static void printAmerican(int s){
+	public static void printAmerican(String day, String month, int date, int year){
 		System.out.println(day + "," + month + date + "," + year);
-		
 		}
+	public static void printEuropean(String day, String month, int date, int year){
+		System.out.println(day + " "+ date+ " " + month + " "+year);
+		}
+	
 	
 	public static void main(String[] args){
 			String day = " Monday ";
 			int date = 14;
 			String month = " September ";
 			int year = 2026;
-			System.out.println(day + date + month +year);
+			printAmerican("Monday", "September", 14, 2026);
+			printEuropean("Monday", "September", 14, 2026);
 		}
 }
