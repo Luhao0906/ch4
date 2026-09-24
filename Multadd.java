a2275afdb92135a30a4224814f0f3b29b973dd44
luhao <2777096@learn.vsb.bc.ca>
2026-09-24 13:32:06 -0700

    Add files via upload

---
+++ Multadd.java
@@ -0,0 +1,14 @@
+public class Multadd{
+	public static double multadd(double a, double b, double c){
+		return a * b +c;
+		}	
+	
+	
+	
+	public static void main(String[] args){
+		System.out.println(multadd(Math.cos(Math.PI/4), 0.5 , Math.sin(Math.PI/4)));
+		
+	}
+		}
+	
+
